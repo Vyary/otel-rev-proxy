09859595173fca77d2ebe69593843d9f47a8476e
Vyary <vyaryw@gmail.com>
2025-07-27 13:55:31 +0000

    change cors

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -1,53 +1,31 @@
 package middleware
 
 import (
-	"encoding/json"
-	"log/slog"
 	"net/http"
-	"os"
 )
 
 func Cors(next http.Handler) http.Handler {
-	AllowedOrigins := []string{"*"}
-
-	originsEnv := os.Getenv("ALLOWED_ORIGINS")
-	if originsEnv != "" {
-		if err := json.Unmarshal([]byte(originsEnv), &AllowedOrigins); err != nil {
-			slog.Error("Failed to Unmarshal ALLOWED_ORIGINS env")
-		}
+	allowedOrigins := map[string]bool{
+		"https://exile-profit.com":      true,
+		"https://poe2.exile-profit.com": true,
 	}
-
-	slog.Info("Allowed Origins", "origins", AllowedOrigins)
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
 
-		if isOriginAllowed(origin, AllowedOrigins) {
+		if allowedOrigins[origin] {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
 		}
 
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusNoContent)
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
 			return
 		}
 
 		next.ServeHTTP(w, r)
 	})
-}
-
-func isOriginAllowed(origin string, allowedOrigins []string) bool {
-	if len(allowedOrigins) == 0 {
-		return false
-	}
-
-	for _, allowedOrigin := range allowedOrigins {
-		if allowedOrigin == "*" || allowedOrigin == origin {
-			return true
-		}
-	}
-	return false
 }
