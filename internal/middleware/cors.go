5dab6eaf1378070e7759d64a0c9a7b4324ca2111
Vyary <vyaryw@gmail.com>
2025-07-27 23:25:45 +0000

    minimize cors

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -5,19 +5,9 @@ import (
 )
 
 func Cors(next http.Handler) http.Handler {
-	allowedOrigins := map[string]bool{
-		"https://exile-profit.com":      true,
-		"https://poe2.exile-profit.com": true,
-	}
-
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		origin := r.Header.Get("Origin")
 
-		if allowedOrigins[origin] {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-		}
-
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
